6d745e61353ea6a6be29211e5ada1a6c75970ebc
Dizans <dizansyu@gmail.com>
2020-01-11 17:29:10 +0800

    accomplish part1:make the tests compile

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub struct KvStore;
+
+impl KvStore{
+    pub fn new() -> Self{
+        KvStore{}
+    }
+
+    pub fn set(&mut self, key: String, value: String){
+        unimplemented!()
+    }
+
+    pub fn get(&mut self, key: String) -> Option<String>{
+        unimplemented!()
+    }
+
+    pub fn remove(&mut self, key: String){
+        unimplemented!()
     }
 }
