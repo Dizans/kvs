44df1d9b50824a393e82134401b6ce7b47dfaf04
Dizans <dizansyu@gmail.com>
2020-01-17 10:01:36 +0800

    implement kvs and add some doc

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,4 +1,52 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version};
+use clap::{App, Arg, SubCommand};
+use kvs::KvStore;
 
-fn main(){
-    println!("Hello, world!");
+fn main() {
+    let mut kvs = KvStore::new();
+
+    let matches = App::new(crate_name!()) //  env!("CARGO_PKG_NAME")
+        .bin_name("kvs")
+        .version(crate_version!()) // env!("CARGO_PKG_VERSION")
+        .author(crate_authors!()) // env!("CARGO_PKG_AUTHORS")
+        .about(crate_description!()) // env!("CARGO_PKG_DESCRIPTION")
+        .subcommand(
+            SubCommand::with_name("set")
+                .arg(Arg::with_name("KEY").index(1).required(true))
+                .arg(Arg::with_name("VALUE").index(2).required(true)),
+        )
+        .subcommand(SubCommand::with_name("get").arg(Arg::with_name("KEY").required(true)))
+        .subcommand(SubCommand::with_name("rm").arg(Arg::with_name("KEY").required(true)))
+        .get_matches();
+
+    if let Some(ref matches) = matches.subcommand_matches("set") {
+        let key = matches.value_of("KEY").unwrap();
+        let value = matches.value_of("VALUE").unwrap();
+        kvs.set(key.to_owned(), value.to_owned());
+        return;
+    }
+
+    if let Some(ref matches) = matches.subcommand_matches("get") {
+        let key = matches.value_of("KEY").unwrap();
+        kvs.get(key.to_owned()).unwrap();
+        return;
+    }
+
+    if let Some(ref matches) = matches.subcommand_matches("rm") {
+        let key = matches.value_of("KEY").unwrap();
+        kvs.remove(key.to_owned());
+        return;
+    }
+
+    // match matches.subcommand_name() {
+    //     Some("set") => {
+    //         KvStore.get(key: String)
+    //     }
+    //     Some("get") => {}
+    //     Some("rm") => {}
+    //     None => {}
+    //     _ => {}
+    // }
+    // println!("Hello, world!");
+    panic!("");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,48 @@
-pub struct KvStore;
+// #![deny(missing_docs)]
+use std::collections::HashMap;
+pub struct KvStore {
+    map: HashMap<String, String>,
+}
 
-impl KvStore{
-    pub fn new() -> Self{
-        KvStore{}
+impl KvStore {
+    pub fn new() -> Self {
+        KvStore {
+            map: HashMap::new(),
+        }
+    }
+    /// set key-value pair into database
+    /// ```rust
+    /// use std::error::Error;
+    /// use kvs::KvStore;
+    ///
+    /// fn main()-> Result<(), Box<dyn Error>>{
+    ///     let mut kv = KvStore::new();
+    ///     kv.set("one".to_owned(), "1".to_owned());
+    ///     Ok(())
+    /// }
+    /// ```
+    pub fn set(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
     }
 
-    pub fn set(&mut self, key: String, value: String){
-        unimplemented!()
+    /// get a value by key.return None if the key is not exists
+    /// ```rust
+    /// use std::error::Error;
+    /// use kvs::KvStore;
+    ///
+    /// fn main() -> Result<(), Box<dyn Error>> {
+    ///     let mut kv = KvStore::new();
+    ///     kv.set("one".to_owned(), "1".to_owned());
+    ///     let v1 = kv.get("one".to_owned());
+    ///     assert_eq!(v1, Some(String::from("1")));
+    ///     Ok(())
+    /// }
+    pub fn get(&mut self, key: String) -> Option<String> {
+        self.map.get(&key).and_then(|v: &String| Some(v.clone()))
     }
 
-    pub fn get(&mut self, key: String) -> Option<String>{
-        unimplemented!()
-    }
-
-    pub fn remove(&mut self, key: String){
-        unimplemented!()
+    /// remove a value from database
+    pub fn remove(&mut self, key: String) {
+        self.map.remove(&key);
     }
 }
