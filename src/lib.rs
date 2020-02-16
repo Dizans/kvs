0263735df45e35c7fe876251111f39620a072364
Dizans <dizansyu@gmail.com>
2020-02-16 12:34:41 +0800

    implement `KvStore::open` function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
 // #![deny(missing_docs)]
 use std::collections::HashMap;
 use std::path::Path;
+use thiserror::Error;
+use serde::{Serialize, Deserialize};
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::fs::OpenOptions;
 
 pub struct KvStore {
     map: HashMap<String, String>,
 }
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum KvsError{
-    Io(std::io::Error),
+    #[error("standard io error")]
+    Io(#[from] std::io::Error),
+    #[error("other error")]
+    NotFound(String),
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Op{
+    SetRec(String, String),
+    RmRec(String),
 }
 
 pub type Result<T> = std::result::Result<T, KvsError>;
@@ -58,6 +72,23 @@ impl KvStore {
     }
 
     pub fn open(path: &Path) -> Result<Self>{
-        unimplemented!();
+        let db_file = OpenOptions::new()
+                        .read(true)
+                        .create(true)
+                        .open(path)?;
+        let mut new_kvs = KvStore::new();
+
+        let mut reader = BufReader::new(db_file); 
+        let mut log_str = String::new();
+        reader.read_to_string(&mut log_str)?;
+        
+        let mut logs = serde_json::Deserializer::from_str(&log_str).into_iter::<Op>();
+        while let Some(Ok(op)) = logs.next() {
+            match op{
+                Op::SetRec(k, v)=> new_kvs.set(k, v)?,
+                Op::RmRec(k) => new_kvs.remove(k)?,
+            }
+        }
+        Ok(new_kvs)
     }
 }
